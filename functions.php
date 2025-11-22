ae7c0146fdc0fad4697e1ac050d02b436b428c23
2417964 <lokendrasah023@gmail.com>
2025-11-22 15:30:17 +0545

    - update book done

--- functions.php
+++ functions.php
@@ -5,4 +5,10 @@ function addBook($mysqli, $title, $author, $genre, $year, $isbn) {
     return $stmt->execute();
 }
 
+function updateBook($mysqli, $id, $title, $author, $genre, $year, $isbn) {
+    $stmt = $mysqli->prepare("UPDATE scifi_books SET title=?, author=?, genre=?, year=?, isbn=? WHERE id=?");
+    $stmt->bind_param("sssiii", $title, $author, $genre, $year, $isbn, $id);
+    return $stmt->execute();
+}
+
 ?>
